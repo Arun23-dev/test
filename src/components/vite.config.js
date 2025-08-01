0f55ad2f9990ddf9e6b4822d93049f7a8c73a296
Arun Chaudhary <arunch23k@gmail.com>
2025-08-02 01:38:26 +0545

    Added the lodash

--- src/components/vite.config.js
+++ src/components/vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
 
 
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-})
+     build: {
+       rollupOptions: {
+         output: {
+           manualChunks: {
+             lodash: ['lodash']
+           }
+         }
+       }
+     }
+});
